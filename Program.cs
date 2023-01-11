c92a545ea438c0fdb34537877be04efa17bcd9f9
Liviu <112979350+PLIVIUT@users.noreply.github.com>
2023-01-11 20:30:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HypotenuseCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter size a, b");
+
+            Console.WriteLine("Enter the a size:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the b size:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double hypotenuse = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("You set a = {0}", a);
+            Console.WriteLine("You set b = {0}", b);
+            Console.WriteLine("The hypotenuse is {0}", hypotenuse);
+
+            Console.ReadKey();
+        }
+    }
+}
